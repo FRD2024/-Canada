262297766ecbb5debbc804e88434caeb96eaafcd
Github <151536032+FRD2024@users.noreply.github.com>
2024-01-28 17:34:23 +0600

    complite

---
+++ app.js
@@ -0,0 +1,23 @@
+        
+        function redirect(){
+            var test =  document.querySelector('#passport');
+          
+                 if(test.value == "A08129109"){
+                     window.location.href="ashrafuzzaman.html";
+                }else if(test.value == "A02493883"){
+                   window.location.href="mamun.html";
+                }else if (test.value == "EL0459102"){
+                         window.location.href="mostafa.html";
+                }else if (test.value == "EG0594873"){
+                         window.location.href="rofequl.html"
+                }else if (test.value == "A08149695"){
+                         window.location.href="akter.html";
+                }else if (test.value == "EE0363038"){
+                         window.location.href="zakir.html";
+                }                
+                                                                                             
+            else{
+                 alert("invalid Passport")
+            }
+   
+              }
