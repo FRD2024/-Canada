ee239ccb5d634f2a3faf8c6c663bbd1a5e0fb4d5
Github <151536032+FRD2024@users.noreply.github.com>
2024-01-29 13:33:00 +0600

    app.js
    Updated

--- app.js
+++ app.js
@@ -9,12 +9,16 @@
                 }else if (test.value == "EL0459102"){
                          window.location.href="mostafa.html";
                 }else if (test.value == "EG0594873"){
-                         window.location.href="rofequl.html"
+                         window.location.href="rofequl.html";
                 }else if (test.value == "A08149695"){
                          window.location.href="akter.html";
                 }else if (test.value == "EE0363038"){
                          window.location.href="zakir.html";
-                }                
+                }else if (test.value == "A03980584" ){
+                         window.location.href="jahangir.html";
+                }else if (test.value == "EB0596393"){
+                         window.location.href="polash.html";
+                }
                                                                                              
             else{
                  alert("invalid Passport")
